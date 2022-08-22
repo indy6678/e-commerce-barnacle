debc9c87cd3ebb65ae3a404eb97b0cee2e3e795d
indy6678 <indy6678@gmail.com>
2022-08-21 22:32:38 -0500

    adding routes

--- config/connection.js
+++ config/connection.js
@@ -7,9 +7,10 @@ const sequelize = process.env.JAWSDB_URL
   : new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
       host: 'localhost',
       dialect: 'mysql',
-      dialectOptions: {
-        decimalNumbers: true,
-      },
+      // port: 3306
+      // dialectOptions: {
+      //   decimalNumbers: true,
+      // },
     });
 
 module.exports = sequelize;
--- server.js
+++ server.js
@@ -1,16 +1,21 @@
-const express = require('express');
-const routes = require('./routes');
+const express = require("express");
+const routes = require("./routes");
 // import sequelize connection
+const sequelize = require("./config/connection");
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
+// express middleware
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+// turn on routes
 app.use(routes);
 
 // sync sequelize models to the database, then turn on the server
-app.listen(PORT, () => {
-  console.log(`App listening on port ${PORT}!`);
+sequelize.sync({ force: false }).then(() => {
+  app.listen(PORT, () => {
+    console.log(`App listening on port ${PORT}!`);
+  });
 });
