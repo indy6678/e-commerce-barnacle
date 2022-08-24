23a9761752ede95a19822514df35bcb572d3faac
indy6678 <indy6678@gmail.com>
2022-08-24 17:46:08 -0500

    updated models

--- config/connection.js
+++ config/connection.js
@@ -7,10 +7,10 @@ const sequelize = process.env.JAWSDB_URL
   : new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
       host: 'localhost',
       dialect: 'mysql',
-      // port: 3306
-      // dialectOptions: {
-      //   decimalNumbers: true,
-      // },
+      port: 3306,
+      dialectOptions: {
+        decimalNumbers: true,
+      },
     });
 
 module.exports = sequelize;
