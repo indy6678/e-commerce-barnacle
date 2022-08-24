95c1afd2fb920999e4eaf2beb3e8df8f4147f5fb
indy6678 <indy6678@gmail.com>
2022-08-24 18:44:16 -0500

    updated associations

--- models/index.js
+++ models/index.js
@@ -6,24 +6,24 @@ const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category
 Product.belongsTo(Category, {
-  foreignKey:"category_id",
+  foreignKey: "category_name",
 });
 
 // Categories have many Products
 Category.hasMany(Product, {
-  foreignKey: "product_id",
+  foreignKey: "product_name",
 });
 
 // Products belongToMany Tags (through ProductTag)
 Product.belongsToMany(Tag, {
   through: ProductTag,
-  foreignKey:"product_id",
+  foreignKey: "product_name",
 });
 
 // Tags belongToMany Products (through ProductTag)
 Tag.belongsToMany(Product, {
   through: ProductTag,
-  foreignKey:"tag_id",
+  foreignKey: "tag_name",
 });
 
 module.exports = {
